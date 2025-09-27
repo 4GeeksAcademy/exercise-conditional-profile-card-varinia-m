f7b8a7aa210123be8eeb0c52ab0ee7dbd2a24bac
Varinia <v.mesias.r@gmail.com>
2025-09-27 03:19:42 +0000

    se corrigio origen del repo

--- src/js/app.js
+++ src/js/app.js
@@ -33,7 +33,7 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-           <h1>${variables.name == null ? "Name" : variables.name}
+            <h1>${variables.name == null ? "Name" : variables.name}
               ${
                 variables.lastName == null ? "LastName" : variables.lastName
               }</h1>
